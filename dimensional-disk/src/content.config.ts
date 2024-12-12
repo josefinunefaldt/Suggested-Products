d82283e3c0a84d22a85a04cbde9887ea069a4025
josefinunefaldt <josefin6@hotmail.com>
2024-12-12 11:36:08 +0100

    init product collection

---
+++ dimensional-disk/src/content.config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from "astro/loaders";
+
+const products = defineCollection({
+  loader: glob({ pattern: "**/*.json", base: "./src/data/products" }),
+  schema: z.object({
+    name: z.string(),
+  }),
+});
+
+export const collections = { products };
